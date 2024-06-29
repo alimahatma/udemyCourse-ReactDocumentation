5af5f308da6c9636ccb67fed6843aa2c74d7cd4f
alimahatma <laluallistilo.com>
2024-06-30 06:57:01 +0800

    add for learning props and state in React

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+
+    state = {
+        name: 'Gibran'
+    }
+
+    changed = evt => {
+        this.setState({ name : evt.target.value});
+        console.log(this.state.name);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 onClick={this.props.myalert}>
+                    { this.props.trademark }
+                </h2>
+                <input value={ this.state.name } onChange={ this.changed } type="text "/>
+            </div>
+        )
+    }
+
+}
+
+export default Footer;
